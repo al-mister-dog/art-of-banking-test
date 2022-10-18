261e02362fd79f998ed6703739717122ec999980
alex hunter <alexhunter@alexs-MacBook-Air.local>
2022-10-18 10:51:08 -0700

    remove faker package

--- src/components/mobile/interactive-ui/charts/multiaxislinechart.tsx
+++ src/components/mobile/interactive-ui/charts/multiaxislinechart.tsx
@@ -9,7 +9,7 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
+
 
 ChartJS.register(
   CategoryScale,
@@ -58,14 +58,14 @@ export const data = {
   datasets: [
     {
       label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
       yAxisID: 'y',
     },
     {
       label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
       yAxisID: 'y1',
@@ -74,5 +74,5 @@ export const data = {
 };
 
 export default function App() {
-  return <Line options={options} data={data} />;
+  return <></>
 }
