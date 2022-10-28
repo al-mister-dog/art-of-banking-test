9c16c0d14fe184e1448a7c3a751423f8a6878060
alex hunter <alexhunter@alexs-MacBook-Air.local>
2022-10-28 16:38:44 -0700

    clearing errors

--- src/domain/services/assets.ts
+++ src/domain/services/assets.ts
@@ -1,15 +1,1 @@
-const Assets = {
-  addProperty(id) {},
-  purchaseProperty(id1, id2) {},
-};
-
-const exampleProperties = {
-    id: 0,
-    accounts: {
-        0: {
-            id: 0,
-            instrument: "Property",
-            balance: 500000,
-        }
-    }
-}
+export const prototype =""
