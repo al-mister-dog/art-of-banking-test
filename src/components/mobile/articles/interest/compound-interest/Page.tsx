1bba3f5d101325c9c4667e84749bef006155e52d
alex hunter <alexhunter@alexs-MacBook-Air.local>
2022-11-04 20:39:31 -0700

    rewrite compound interest article

--- src/components/mobile/articles/interest/compound-interest/Page.tsx
+++ src/components/mobile/articles/interest/compound-interest/Page.tsx
@@ -23,37 +23,41 @@ export default function CompoundInterestPage() {
       </Box>
       <Text>
         Compound interest is a form of interest on a loan or investment that
-        grows in intervals of time. The idea of compound interest has been
-        around for thousands of years in various forms. The first part of this
-        series will look solely at the mechanics of compound interest, and later
-        we will look at compound interest in various social and historical
-        contexts. We will define the very basic principles of interest formulas
-        and learn to calculate compound interest using different variables.
+        grows over time. The idea of compound interest has been around for
+        thousands of years in various forms. The first part of this series will
+        look solely at the mechanics of compound interest, and later we will
+        look at compound interest in various social and historical contexts. We
+        will define the very basic principles of interest formulas and learn to
+        calculate compound interest using different variables.
       </Text>
       <br></br>
       <Center>
         <SubTitle>Simple Interest</SubTitle>
       </Center>
       <Text>
-        To understand the difference between compound interest and regular
-        interest lets define simple interest and look at a simple interest
-        calculator. The interest rate is simply a percentage of the initial
-        amount. For example if the initial amount is 100 and the interest rate
-        is 5, the amount of interest would also be 5.
+        To understand what makes compound interest different from other kinds of
+        interest, we will first define simple interest and then take look at a
+        simple interest calculator. With simple interest, the amount of interest
+        applied to a loan is worked out by applying the{" "}
+        <strong>interest rate</strong>. The interest rate is simply a percentage
+        of the initial amount that is to be invested/loaned. For example if the
+        initial amount of a loan was $100 and the interest rate was 5 percent,
+        the amount of interest would be %5.
       </Text>
       <SimpleInterestFormula />
       <br></br>
       <Text>
-        Therefore the total amount after interest is the initial amount plus the
-        interest.
+        Therefore the sum total amount including simple interest is the initial
+        amount plus the interest.
       </Text>
       <TotalInterestFormula />
       <br></br>
       <Text>
-        However this is just the interest added to the total for one time.
-        Usually, interest will be added more than once. If a loan was left
-        unpaid for ten years, then the amount of interest will multiply by ten
-        also. If the interest was ten percent, then this would amount to a
+        However this is just the interest added to the total for one turn.
+        Usually, interest will be added more than once during the lifespan of a
+        loan or investment. If a loan was left unpaid for ten years, then the
+        amount of interest will multiply by ten also. If the interest rate was
+        ten percent, the amount of simple interest added would amount to a
         doubling of the intitial amount.
       </Text>
       <SimpleInterestIteration />
@@ -68,26 +72,27 @@ export default function CompoundInterestPage() {
         <SubTitle>Compound Interest</SubTitle>
       </Center>
       <Text>
-        Unlike simple interest, compound interest cannot be formulated
-        initially. Compound interest starts off exactly the same as simple
-        interest does, with interest being the percentage of the principal{" "}
+        Unlike simple interest, compound interest increases with every turn,
+        whether annually or monthly etc. Compound interest starts off exactly
+        the same as simple interest does, with interest being the percentage of
+        the principal{" "}
         <span style={{ fontStyle: "italic" }}>
           (from now on, we will refer to the initial amount as the{" "}
           <strong>principal</strong>)
         </span>
-        . What compound interest does is take the new total and use that as the
-        new number from which to apply calculation of the interest. If the
-        principal was 100 and the interest rate was 5%, the sum total on the
-        first iteration would be 105. But now we take the interest to be 5% of
-        105 instead of 100, and apply that interest to the sum total the second
-        go round. Lets apply this concept to our previous examples.
+        . What compound interest does is take the first sum total and use that
+        as the new amount from which to apply the second calculation of the
+        interest. If the principal was 100 and the interest rate was 5%, the sum
+        total on the first iteration would be 105. But now we take the interest
+        to be 5% of 105 instead of 100, and apply that interest to the sum total
+        the second go round. Lets apply this concept to our previous examples.
       </Text>
       <CompoundInterestFormula />
       <br></br>
       <Text>
         Lets compare the sum totals of simple interest and compound interest
         over time. You will see that the difference between the two becomes more
-        extreme the larger the amount of times that interest is compounded.
+        extreme the more number of times that interest is compounded.
       </Text>
       <CompoundInterestIteration />
       <br></br>
@@ -106,11 +111,14 @@ export default function CompoundInterestPage() {
         each year at a certain percentage, so over ten years of annually
         compounding interest, there are ten compounding intervals. However many
         loans and investments have interests that compound multiple times a
-        year, for example twice a year. This would mean that over ten years,
-        there would be twenty compounding intervals. This would raise the sum
-        total by an extreme amount, so the the interest rate is also divided by
-        the number of compound intervals in a year. So with an interest rate of
-        10 and a semi-annual compound interval, the interest rate would be 5.
+        year, for example twice or four times a year. This would mean that given
+        a semi-annual compounding interval over ten years, there would be twenty
+        compounding intervals. This would seemingly raise the sum total by an
+        extreme amount, but the the interest rate is also divided by the number
+        of compound intervals in a year meaning the amount of interest added on
+        each compounding interval is smaller. So given an interest rate of 10%
+        and a semi-annual compound interval, the interest rate would be 5
+        (10/2).
       </Text>
       <CompoundIntervalFormula />
       <br></br>
@@ -149,10 +157,10 @@ export default function CompoundInterestPage() {
         As just mentioned, this is deceptively simple. You may have noticed that
         if you set the inflation rate higher than the interest rate you end up
         with negative figures. This means that an investor can stand to lose
-        purchasing power from an investment. It also means that debtors can use
-        inflation to pay off debts. Real interest is often factored into
-        government borrowing for this reason. Also no one knows what the
-        inflation rate is going to be over time. This brings up lots of
+        purchasing power over the lifespan of an investment. It also means that
+        debtors can use inflation to pay off debts. Real interest is often
+        factored into government borrowing for this reason. Also no one knows
+        what the inflation rate is going to be over time. This brings up lots of
         questions which will be explored in another article. Below is a compound
         interest calculator to find the sum total of a loan/investment that has
         been incremented at various intervals annually, as well as being
