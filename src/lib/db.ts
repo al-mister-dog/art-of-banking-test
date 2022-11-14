0253faf1dea48f800c9059c36908ba1feae4071b
alex hunter <alexhunter@alexs-MacBook-Air.local>
2022-11-13 19:19:26 -0800

    env var

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(
-    "mongodb+srv://almrdog:Coltrane67@cluster0.uglli.mongodb.net/?retryWrites=true&w=majority"
+    process.env.DATABASE_URL
   );
 
   return client;
