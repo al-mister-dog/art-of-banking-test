54c65d43ad16862c34b980fedbc43d4be27dea1e
alex hunter <alexhunter@alexs-MacBook-Air.local>
2022-11-13 19:40:31 -0800

    startup

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,1 @@
-import { MongoClient } from "mongodb";
-
-export async function connectToDatabase() {
-  const client = await MongoClient.connect(
-    process.env.DATABASE_URL
-  );
-
-  return client;
-}
+export const postgresurl = "postgres://postgres:Coltrane67@localhost:5432/aob";
